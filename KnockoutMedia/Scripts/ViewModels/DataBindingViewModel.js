398f7197a7c9684b7324cad197ffff6ebd6a4ecf
okiselov <kiselov.oleksandr@apriorit.com>
2018-06-24 19:30:30 +0300

    Two way data-binding using elements: input, select, multiselect, radiobutton, checkbox and submit form

--- KnockoutMedia/Scripts/ViewModels/DataBindingViewModel.js
+++ KnockoutMedia/Scripts/ViewModels/DataBindingViewModel.js
@@ -2,9 +2,16 @@
 app.DataBindingViewModel = (function (ko) {
     'use strict';
     var me = {
+        chosenColors: ko.observableArray([]),
         message: ko.observable(''),
+        color: ko.observable(''),
         colors: ["Red", "Green", "Blue", "Yellow", "Black", "White"],
-        loadMessage: loadMessage
+        isChecked: ko.observable(false),
+        keyValuePair: ko.observable(undefined),
+        keyValuePairs: [{ Key: "First", Value: "1" }, { Key: "Second", Value: "2" }, { Key: "Third", Value: "3" }, { Key: "Fourth", Value: "4" }],
+        ordinal: ko.observable(undefined),
+        loadMessage: loadMessage,
+        showMessage: showMessage
     };
     function _init() {
         me.message.subscribe(function (v) {
@@ -14,6 +21,10 @@ app.DataBindingViewModel = (function (ko) {
 
     function loadMessage() {
         me.message("My favorite color is: " + me.colors[Math.floor(Math.random() * me.colors.length)].toLowerCase());
+    }
+
+    function showMessage() {
+        alert(me.message() ? me.message() : 'Please type the message!')
     }
 
     _init ();
