7d9a748690eb40317735c468f135f1e12fd3197f
okiselov <kiselov.oleksandr@apriorit.com>
2018-06-24 18:32:39 +0300

    Simple two way data binding is established - data-binding.html

---
+++ KnockoutMedia/Scripts/ViewModels/DataBindingViewModel.js
@@ -0,0 +1,21 @@
+﻿var app = window.app || {};
+app.DataBindingViewModel = (function (ko) {
+    'use strict';
+    var me = {
+        message: ko.observable(''),
+        colors: ["Red", "Green", "Blue", "Yellow", "Black", "White"],
+        loadMessage: loadMessage
+    };
+    function _init() {
+        me.message.subscribe(function (v) {
+            //alert("Your message is: " + v);
+        });
+    }
+
+    function loadMessage() {
+        me.message("My favorite color is: " + me.colors[Math.floor(Math.random() * me.colors.length)].toLowerCase());
+    }
+
+    _init ();
+    return me;
+}(ko));
