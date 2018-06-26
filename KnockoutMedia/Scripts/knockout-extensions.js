55c1cd4b20c1005e24e762383692d54ac0332c50
okiselov <kiselov.oleksandr@apriorit.com>
2018-06-26 10:39:32 +0300

    Custom functions

---
+++ KnockoutMedia/Scripts/knockout-extensions.js
@@ -0,0 +1,15 @@
+﻿ko.observableArray.fn.filterByProperty = function (property, value) {
+    return ko.pureComputed(function () {
+        var items = this(),
+            matches = [],
+            current = null,
+            i, l;
+        for (i = 0, l = items.length; i < l; i++) {
+            current = items[i];
+            if (ko.unwrap(current[property]) === value) {
+                matches.push(current);
+            }
+        }
+        return matches;
+    }, this);
+};
